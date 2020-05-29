d1b77ca86f26dcafeb4a069a44ffa7d5ca41a21a
Robert Messerle <robert.messerle@reddit.com>
2020-05-29 16:13:21 -0400

    converts SplitSection

---
+++ src/components/SplitSection/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import css from './index.module.scss';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+function SplitSection({ children }: Props) {
+  return <div className={css.root}>{children}</div>;
+}
+
+export default React.memo(SplitSection);
