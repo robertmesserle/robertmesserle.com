39a3ee4193283837f7f009e79f556db0eef1decc
Robert Messerle <robert.messerle@reddit.com>
2020-05-29 16:45:19 -0400

    syntax cleanup: Contact

--- src/components/pages/Contact/index.tsx
+++ src/components/pages/Contact/index.tsx
@@ -7,30 +7,35 @@ import SplitSection from 'components/SplitSection';
 import useAutogrow from 'hooks/useAutogrow';
 import css from './index.module.scss';
 
-const Contact: React.FC = () => (
-  <Page header='contact' subheader='Robert Messerle'>
-    <Section>
-      <SplitSection>
-        <Field label='Your name' placeholder='John Doe' type='text' />
-        <Field label='Your email' placeholder='john.doe@domain.com' type='email' />
-      </SplitSection>
-      <Field label='Your message' type='textarea' />
-      <SplitSection>
-        <input className={css.inputButton} type='reset' value='Clear' />
-        <input className={css.inputButton} type='submit' value='Send' />
-      </SplitSection>
-    </Section>
-  </Page>
-);
+function Contact() {
+  return (
+    <Page header='contact' subheader='Robert Messerle'>
+      <Section>
+        <SplitSection>
+          <Field label='Your name' placeholder='John Doe' type='text' />
+          <Field label='Your email' placeholder='john.doe@domain.com' type='email' />
+        </SplitSection>
+        <Field label='Your message' type='textarea' />
+        <SplitSection>
+          <input className={css.inputButton} type='reset' value='Clear' />
+          <input className={css.inputButton} type='submit' value='Send' />
+        </SplitSection>
+      </Section>
+    </Page>
+  );
+}
 
 export default Contact;
 
-interface TextInputProps {
+function Field({
+  label,
+  placeholder,
+  type,
+}: {
   label: string;
   placeholder?: string;
   type: 'text' | 'textarea' | 'email';
-}
-const Field: React.FC<TextInputProps> = ({ label, placeholder, type }) => {
+}) {
   const autogrowRef = useAutogrow();
 
   return (
@@ -43,4 +48,4 @@ const Field: React.FC<TextInputProps> = ({ label, placeholder, type }) => {
       )}
     </label>
   );
-};
+}
